ea16886b73fc63465eb218b232ad51d1f7b5f50b
saijog-07 <111268358+saijog-07@users.noreply.github.com>
2025-06-13 16:17:59 +0530

    Add files via upload

---
+++ Hibernate_CRUD_HQL/src/main/java/com/Hibernate2/App.java
@@ -0,0 +1,25 @@
+package com.Hibernate2;
+
+import service.StudentService;
+import dao.DaoStudentI;
+
+public class App {
+    public static void main(String[] args) {
+        StudentService service = new StudentService();
+        // service.saveStudent();  
+        // service.removeData(); 
+        // service.updateStudent();
+        // service.searchRoll();
+        
+//        DaoStudentI dao = new DaoStudentI();
+//        dao.ShowData("Sai");
+        
+       // service.searchName();
+        
+       // service.searchNameMarks();
+       // service.searchUseMarks();
+       // service.removeUsingRoll();
+       // service.updateNameByRoll();
+        service.displayData();
+    }
+}
